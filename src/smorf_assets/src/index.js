6fd24569d58a5164bc19ea2df8e019b5447f57a2
ioustamora <ioustamora@protonmail.com>
2022-06-01 16:04:16 +0300

    init

---
+++ src/smorf_assets/src/index.js
@@ -0,0 +1,19 @@
+import { smorf } from "../../declarations/smorf";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await smorf.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
